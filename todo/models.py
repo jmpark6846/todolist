9ebe4e1eb0d358443e3b405e55c3fa5260ff7ae8
jmpark6846 <jmpark6846@github.com>
2018-10-30 11:09:36 +0900

    todo 모델 추가; 테스트 통과

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField('제목', max_length=255)
+    content = models.TextField('내용')
+
+    def __str__(self):
+        return self.title
