23bb7ac36582ae736050ad83b335ae93ef51d3b2
jmpark6846 <jmpark6846@github.com>
2018-10-30 11:01:17 +0900

    todo 모델 테스트 추가

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+class TodoModelTestCase(TestCase):
+    def test_model_can_make_an_instance(self):
+        old_count = Todo.objects.count()
+        Todo.objects.create(title="test title", content="this is new content")
+        new_count = Todo.objects.count()
+        self.assertNotEqual(old_count,new_count)
+
