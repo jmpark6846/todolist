21f133caf0757f0c96ec1a7e7aa08a04e1dede9d
jmpark6846 <jmpark6846@github.com>
2018-11-01 12:54:18 +0900

    자잘한 수정

--- proj/settings/prod.py
+++ proj/settings/prod.py
@@ -15,22 +15,5 @@ ALLOWED_HOSTS = [
     'localhost',
     'wintercoding-todolist.herokuapp.com'
 ]
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+
 django_heroku.settings(locals())
