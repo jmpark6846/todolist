86e9f03c04501c870e294fa7578c7aef3407e159
jmpark6846 <jmpark6846@github.com>
2018-11-01 10:54:08 +0900

    django-heroku 추가

--- proj/settings.py
+++ proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,3 +137,6 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = 'staticfiles'
+
+
+django_heroku.settings(locals())
