976e2d764701bd37161ecffea336e70aa06bcd1d
jmpark6846 <jmpark6846@github.com>
2018-11-02 23:44:28 +0900

    user login, signup template 수정

--- user/forms.py
+++ user/forms.py
@@ -1,8 +1,32 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.ModelForm):
-  class Meta:
-    model = User
-    fields = ['username', 'password']
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': '아이디',
+                'class': 'form-control',
+            }),
+            'password': forms.TextInput(attrs={
+                'placeholder': '패스워드',
+                'class': 'form-control',
+            }),
+        }
 
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': '아이디',
+                'class': 'form-control',
+            }),
+            'password': forms.TextInput(attrs={
+                'placeholder': '패스워드',
+                'class': 'form-control',
+            }),
+        }
